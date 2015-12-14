5a521a0cbb2e3cee57eef459d6c7ac7c793050e7
Michael Persson <michael.ake.persson@gmail.com>
2015-12-14 16:36:31 +0100

    Add site_refs to python lib

--- python_client/client.py
+++ python_client/client.py
@@ -17,36 +17,36 @@ class Client:
             resp = requests.get(Client.url + "/subnets?envelope=false")
             objlist = []
             for e in resp.json():
-                objlist.append(Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"]))
+                objlist.append(Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"], e["siteRef"]))
             return objlist
 
         def get(self, name):
             resp = requests.get(Client.url + "/subnets/{0}?envelope=false".format(name))
             e = resp.json()
-            return Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"])
+            return Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"], e["siteRef"])
 
         def get_by_id(self, id):
             resp = requests.get(Client.url + "/subnets/id/{0}?envelope=false".format(id))
             e = resp.json()
-            return Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"])
-
+            return Subnet(e["id"], e["subnet"], e["mask"], e["gw"], e["siteId"], e["siteRef"])
 
 class Subnet:
-    def __init__(self, id, subnet, mask, gw, site_id):
+    def __init__(self, id, subnet, mask, gw, site_id, site_ref):
         self.id = id
         self.subnet = subnet
         self.mask = mask
         self.gw = gw
         self.site_id = site_id
+        self.site_ref = site_ref
 
 clnt = Client()
 data = clnt.Subnet().all()
 
 for obj in data:
-    print obj.mask, obj.gw, obj.site_id
+    print obj.id, obj.subnet, obj.mask, obj.gw, obj.site_id, obj.site_ref
 
 obj = clnt.Subnet().get("192.168.0.0-24")
-print obj.mask, obj.gw, obj.site_id
+print obj.id, obj.subnet, obj.mask, obj.gw, obj.site_id, obj.site_ref
 
 obj = clnt.Subnet().get_by_id(obj.id)
-print obj.mask, obj.gw, obj.site_id
+print obj.id, obj.subnet, obj.mask, obj.gw, obj.site_id, obj.site_ref
