0be4f99184f7cfb4e1ce62f11dc56f245bc1d2b2
Michael Persson <michael.ake.persson@gmail.com>
2016-01-12 11:51:27 +0100

    Add support for querying boolean in hosts

--- api/src/github.com/imc-trading/dock2box/api/controllers/common.go
+++ api/src/github.com/imc-trading/dock2box/api/controllers/common.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-// structTags return ac onversion map of tags for a struct
+// structTags return a conversion map of tags for a struct
 func structTags(val reflect.Value, fromTag string, toTag string) (map[string]string, error) {
 	if val.Kind() != reflect.Struct {
 		return nil, fmt.Errorf("unsupported type: %s needs to be a Struct", val.Kind())
@@ -20,4 +20,20 @@ func structTags(val reflect.Value, fromTag string, toTag string) (map[string]str
 	}
 
 	return keys, nil
+}
+
+// structKinds return a conversion map of kind for tags in a struct
+func structKinds(val reflect.Value, tag string) (map[string]reflect.Kind, error) {
+	if val.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("unsupported type: %s needs to be a Struct", val.Kind())
+	}
+
+	kinds := make(map[string]reflect.Kind)
+	for i := 0; i < val.NumField(); i++ {
+		t := val.Type().Field(i)
+		k := t.Tag.Get(tag)
+		kinds[k] = t.Type.Kind()
+	}
+
+	return kinds, nil
 }
