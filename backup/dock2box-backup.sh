11165ccd83b2eade0412980c234f39344e0383c9
Michael Persson <michael.ake.persson@gmail.com>
2016-02-12 11:26:33 +0100

    Diff backups only keep one's that add content

--- backup/dock2box-backup.sh
+++ backup/dock2box-backup.sh
@@ -7,4 +7,13 @@ DIR="/var/lib/dock2box/backups"
 [ -d "${DIR}" ] || mkdir -p ${DIR}
 
 DATETIME=$(date +'%Y%m%d%H%M%S')
-etcdtool -p http://localhost:2379 export / | gzip >${DIR}/${DATETIME}.json.gz
+etcdtool -p http://localhost:2379 export / >${DIR}/${DATETIME}.json
+
+if test -e ${DIR}/last.json && diff ${DIR}/last.json ${DIR}/${DATETIME}.json; then
+    rm -f ${DIR}/${DATETIME}.json
+    echo "No new content, won't keep backup"
+    exit 0
+fi
+
+rm -f ${DIR}/last.json
+ln -s ${DIR}/${DATETIME}.json ${DIR}/last.json
